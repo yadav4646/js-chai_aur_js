0f0b02538e25600625bdf580d1fce8ce0984cb79
Kuldeep Yadav <64552714+yadav4646@users.noreply.github.com>
2023-10-10 04:45:33 +0530

    Add files via upload

---
+++ ChaiAurCodeJS/01_basics/dataTypes-summary.js
@@ -0,0 +1,26 @@
+//JS is a Dynamically 
+
+// Primitive
+
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol(Unique value), BigInt 
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id == anotherId);
+// console.log(id === anotherId);
+
+
+
+// Refrence(Non-Primitive)
+
+// Arrays, Objects, Functions
+
+const heros = ["shark", "spider", "lion"] // Arrays
+
+const animals = { name: "Kuldeep, age: 27" } // objects // Key-value pair
+
+const myFunction = function () {
+    console.log("Hello");
+}
