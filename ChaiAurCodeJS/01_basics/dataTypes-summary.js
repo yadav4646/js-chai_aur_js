81532460b484a56ba36d26119776c5239bcf04b5
Kuldeep Yadav <64552714+yadav4646@users.noreply.github.com>
2023-10-10 08:28:12 +0530

    Add files via upload

--- ChaiAurCodeJS/01_basics/dataTypes-summary.js
+++ ChaiAurCodeJS/01_basics/dataTypes-summary.js
@@ -11,16 +11,53 @@
 // console.log(id == anotherId);
 // console.log(id === anotherId);
 
-
-
-// Refrence(Non-Primitive)
+// Reference(Non-Primitive)
 
 // Arrays, Objects, Functions
 
-const heros = ["shark", "spider", "lion"] // Arrays
+// const heros = ["shark", "spider", "lion"] // Arrays
 
-const animals = { name: "Kuldeep, age: 27" } // objects // Key-value pair
+// const animals = { name: "Kuldeep, age: 27" } // objects // Key-value pair
 
-const myFunction = function () {
-    console.log("Hello");
+// const myFunction = function () {
+//     console.log("Hello");
+// }
+
+
+
+
+
+//    Memory (Heap and Stack)
+
+// Stack
+// Primitive data types and references
+// Size is known at compile time 
+// Fixed memory allocated
+
+// Heap
+// Objects and functions
+// Size is known at run time
+// No limit for object memory
+
+
+// Stack(Primitives uses stack)
+// Heap(Non-primitives uses heap)
+
+let myYoutubeName = "Kuldeep"
+let anotherName = myYoutubeName
+anotherName = "abhishek"
+
+console.log(myYoutubeName)
+console.log(anotherName);
+
+let user1 = {
+    email: "yada@gmail.com",
+    upi: "user@ybl"
 }
+
+let user2 = user1
+
+user2.email = "kuldeep@googe.com"
+
+console.log(user1.email);
+console.log(user2.email);
