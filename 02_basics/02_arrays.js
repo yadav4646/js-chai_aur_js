8a45c0272881213972463905b044a71155c78803
Kuldeep Yadav <64552714+yadav4646@users.noreply.github.com>
2023-10-13 16:43:04 +0530

    Add files via upload

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,32 @@
+const marvelHeroes = ["thor", "Ironman", "SipiderMan"]
+const dcHeroes = ["Superman", "flash", "bataman"]
+
+// marvelHeroes.push(dcHeroes)
+
+// console.log(marvelHeroes);
+// console.log(marvelHeroes[3]);
+// console.log(marvelHeroes[3][1]);
+
+// const allHeores = marvelHeroes.concat(dcHeroes)
+// console.log(allHeores);
+
+// const allNewHeores = [...marvelHeroes, ...dcHeroes]
+
+// console.log(allNewHeores);
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+// let real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+
+// console.log((Array.isArray("Kuldeep")));
+// console.log((Array.from("Kuldeep")));
+// console.log((Array.from({ name: "Kuldeep" })));   // Interesting case, returns an empty arr as we have to give the key value
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+
+// console.log(Array.of(score1, score2, score3
+// ));
