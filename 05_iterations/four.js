1ccf998a294c3061eb9637faa5fd485f6c46f7ac
Kuldeep Yadav <64552714+yadav4646@users.noreply.github.com>
2023-10-21 18:55:25 +0530

    Add files via upload

---
+++ 05_iterations/four.js
@@ -0,0 +1,26 @@
+const myObj = {
+    js: "Javascript",
+    cpp: 'C++',
+    rb: 'Ruby',
+    swift: 'Swift'
+}
+
+// for (const key in myObj) {
+//     console.log(`${key} shortcut is for ${myObj[key]}`);
+// }
+
+// const programming = ['js', 'Python', 'Java', 'Cpp']
+// for (const key in programming) {
+//     console.log(`${programming[key]} all are languages`);
+// }
+
+//**Maps is not ittreatable by for-loop */
+// const map = new Map()
+// map.set('In', 'India')
+// map.set('USA', 'United states of America')
+// map.set('FR', 'France')
+// map.set('In', 'India')
+
+// for (const key in map) {
+//     console.log(key);
+// }
